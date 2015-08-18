ff3780ac906dfb1025afdda36686e1add9d4317d
vicmatmar <victormartin@hotmail.com>
2015-08-18 16:39:53 -0500

    Started a telnet session to zebrashark, send cu cs5480_pload command and parse output for raw current and voltage registers values

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinimalisticTelnet;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication1
+{
+    class TelnetTest
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //create a new telnet connection
+                TelnetConnection tc = new TelnetConnection("localhost", 4900);
+
+                bool isconnected = tc.IsConnected;
+
+                tc.WriteLine("cu cs5480_pload");
+                string datain = tc.Read();
+
+                string rawCurrentPattern = "Raw IRMS: ([0-9,A-F]{8})";
+                string rawVoltagePattern = "Raw VRMS: ([0-9,A-F]{8})";
+
+                Match match = Regex.Match(datain, rawCurrentPattern);
+                string current_hexstr = match.Groups[1].Value;
+                match = Regex.Match(datain, rawVoltagePattern);
+                string voltage_hexstr = match.Groups[1].Value;
+
+                while (true)
+                {
+
+                    datain = tc.Read();
+                }
+
+            }
+        }
+    }
+}
