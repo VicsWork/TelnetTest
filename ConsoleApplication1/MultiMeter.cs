0cfc6a9b599cffc984359341bbb46563c7469605
vicmatmar <victormartin@hotmail.com>
2015-08-18 17:20:08 -0500

    added meter measurement

---
+++ ConsoleApplication1/MultiMeter.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Threading;
+
+namespace powercal
+{
+    class MultiMeter
+    {
+        public bool WaitForDsrHolding
+        {
+            get { return _waitForDsrHolding; }
+            set { _waitForDsrHolding = value; }
+        }
+
+        private bool _waitForDsrHolding = true;
+        private string _portName;
+        private SerialPort _serialPort = new SerialPort();
+        private string _value_txt = "";
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="portName"></param>
+        public MultiMeter(string portName)
+        {
+            this._portName = portName;
+        }
+
+        /// <summary>
+        /// Open serial port
+        /// </summary>
+        /// <returns></returns>
+        public SerialPort OpenComPort()
+        {
+            //if (_serialPort != null && _serialPort.IsOpen)
+            //{
+            _serialPort.Close();
+            //}
+            //_serialPort = new SerialPort(_portName, 600, Parity.None, 8, StopBits.One);
+            _serialPort.PortName = _portName;
+            _serialPort.BaudRate = 9600;
+            _serialPort.Parity = Parity.None;
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.Handshake = Handshake.None;
+            _serialPort.DtrEnable = true;
+
+            _serialPort.DataReceived += _serialPort_DataReceived;
+            _serialPort.Open();
+
+            return _serialPort;
+        }
+
+        /// <summary>
+        /// Handle data recieved
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            lock (_value_txt)
+            {
+                _value_txt += _serialPort.ReadExisting();
+            }
+        }
+
+        /// <summary>
+        /// Waits for data
+        /// </summary>
+        /// <returns></returns>
+        string waitForData()
+        {
+            int n = 0;
+            while (_value_txt == "")
+            {
+                Thread.Sleep(100);
+                n++;
+                if (n > 5)
+                {
+                    break;
+                }
+            }
+            n = 0;
+            while (_serialPort.BytesToRead > 0)
+            {
+                Thread.Sleep(250);
+                n++;
+                if (n > 10)
+                {
+                    break;
+                }
+            }
+
+            return _value_txt;
+        }
+
+        /// <summary>
+        /// Clears our data holder
+        /// </summary>
+        void clearData()
+        {
+            lock (_value_txt)
+                _value_txt = "";
+        }
+
+        /// <summary>
+        /// Writes to meter
+        /// </summary>
+        /// <param name="cmd"></param>
+        public void writeLine(string cmd)
+        {
+            int n = 0;
+
+            if (_waitForDsrHolding)
+            {
+                while (!_serialPort.DsrHolding)
+                {
+                    Thread.Sleep(250);
+                    n++;
+                    if (n > 20)
+                        throw new Exception("Multimeter not responding to serial commands.  Make sure multi-meter is on and serial cable connected");
+                }
+            }
+
+            _serialPort.WriteLine(cmd);
+            Thread.Sleep(250);
+
+            n = 0;
+            while (_serialPort.BytesToWrite > 0)
+            {
+                Thread.Sleep(100);
+                n++;
+                if (n > 20)
+                    throw new Exception("Multimeter write buffer not empty");
+            }
+        }
+
+        /// <summary>
+        /// Clears the meters error status
+        /// </summary>
+        public void ClearError()
+        {
+            writeLine("*CLS");
+        }
+
+        /// <summary>
+        /// Sets meter to remote mode
+        /// </summary>
+        public void SetToRemote()
+        {
+            writeLine("SYST:REM");
+        }
+
+        /// <summary>
+        /// Gets the meter id
+        /// </summary>
+        /// <returns>meter id string</returns>
+        public string IDN()
+        {
+            clearData();
+            writeLine("*IDN?");
+            string data = waitForData();
+            return data;
+        }
+
+        /// <summary>
+        /// Sets up the meter for V AC measurement
+        /// </summary>
+        public void SetupForVAC()
+        {
+            writeLine(":CONF:VOLT:AC 1000,0.01");
+            writeLine(":TRIG:SOUR BUS");
+        }
+
+        /// <summary>
+        /// Sets up the meter for I AC measurement
+        /// </summary>
+        public void SetupForIAC()
+        {
+            writeLine(":CONF:CURR:AC 1,0.000001");
+            writeLine(":TRIG:SOUR BUS");
+        }
+
+        /// <summary>
+        /// Trigers meter and returns measurement
+        /// </summary>
+        /// <returns>measurement</returns>
+        public string Measure()
+        {
+            clearData();
+            writeLine(":INIT");
+            writeLine("*TRG");
+            writeLine(":FETC?");
+            string data = waitForData();
+            return data;
+        }
+
+        /// <summary>
+        /// Closes the serial port
+        /// </summary>
+        public void CloseSerialPort()
+        {
+            this._serialPort.Close();
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MinimalisticTelnet;
+using powercal;
 using System.Text.RegularExpressions;
 
 namespace ConsoleApplication1
@@ -27,16 +28,61 @@ namespace ConsoleApplication1
 
                 Match match = Regex.Match(datain, rawCurrentPattern);
                 string current_hexstr = match.Groups[1].Value;
+                int current_int = Convert.ToInt32(current_hexstr, 16);
+                double current = RegHex_ToDouble(current_int);
+                current = current * 15 / 0.6;
+
                 match = Regex.Match(datain, rawVoltagePattern);
                 string voltage_hexstr = match.Groups[1].Value;
+                int volatge_int = Convert.ToInt32(voltage_hexstr, 16);
+                double voltage = RegHex_ToDouble(volatge_int);
+                voltage = voltage * 240 / 0.6;
 
-                while (true)
-                {
 
-                    datain = tc.Read();
-                }
+                MultiMeter meter = new MultiMeter("COM1");
+                meter.OpenComPort();
 
+                meter.SetupForIAC();
+                string meter_current_str = meter.Measure();
+                double meter_current = Double.Parse(meter_current_str);
+
+                meter.SetupForVAC();
+                string meter_voltage_str = meter.Measure();
+                double meter_voltage = Double.Parse(meter_voltage_str);
+
+
+
+                meter.CloseSerialPort();
+                int test = 1;
             }
+
+            /// <summary>
+            /// Converts a 24bit hex (3 bytes) CS register value to a double
+            /// </summary>
+            /// <example>
+            /// byte[] rx_data = new byte[3];
+            /// rx_data[2] = 0x5c;
+            /// rx_data[1] = 0x28;
+            /// rx_data[0] = 0xf6;
+            /// Should return midrange =~ 0.36
+            /// </example>
+            /// <param name="rx_data">data byte array byte[2] <=> MSB ... byte[0] <=> LSB</param>
+            /// <returns>range 0 <= value < 1.0</returns>
+            private static double RegHex_ToDouble(int data)
+            {
+                // Maximum 1 =~ 0xFFFFFF
+                // Max rms 0.6 =~ 0x999999
+                // Half rms 0.36 =~ 0x5C28F6
+                double value = ((double)data) / 0x1000000; // 2^24
+                return value;
+            }
+
+            private static double RegHex_ToDouble(string hexstr)
+            {
+                int val_int = Convert.ToInt32(hexstr, 16);
+                return RegHex_ToDouble(val_int); ;
+            }
+
         }
     }
 }
